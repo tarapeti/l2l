d745890b4df9bf8be22f4a1128ea7670920680f4
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-04 12:15:36 +0100

    equals method overriden

--- l2l.Data/Model/Course.cs
+++ l2l.Data/Model/Course.cs
@@ -6,5 +6,22 @@ namespace l2l.Data.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Course);
+
+        }
+
+        public bool Equals(Course course)
+        {
+            if(course == null){
+                return false;
+            }
+            if(Id != course.Id || Name != course.Name){
+                return false;
+            }
+            return true;
+        }
     }
 }
