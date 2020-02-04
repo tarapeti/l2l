d06de839d497c352aad75247a87f63c7187c67cf
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-04 11:40:44 +0100

    created tests repo structure

---
+++ l2l.Data.Tests/CourseRepositoryTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace l2l.Data.Tests
+{
+    /// <summary>
+    /// CRUD and list tests
+    /// </summary>
+    public class CoureseRepositoryTests
+    {
+        [Fact]
+        public void CoureseRepositoryTests_CreateCourseShouldBeInRepository()
+        {
+
+        }
+    }
+}
