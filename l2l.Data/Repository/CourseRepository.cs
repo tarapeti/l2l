dca0ed5fa10b48b521b10ba0411a2245f10b3039
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-04 12:06:43 +0100

    created CourseRepository

---
+++ l2l.Data/Repository/CourseRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using l2l.Data.Model;
+
+namespace l2l.Data.Repository
+{
+    public class CourseRepository
+    {
+        public void Add(Course course)
+        {
+            
+        }
+
+        public Course GetById(int id)
+        {
+            return new Course();
+        }
+    }
+}
