9bc943f7b572affedd36255f6a40ebf0d86fe8e0
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-04 13:58:35 +0100

    basepath, environment variables added, config builder reads appconfig.json

--- l2l.Data/Model/L2lDbContextFactory.cs
+++ l2l.Data/Model/L2lDbContextFactory.cs
@@ -1,6 +1,8 @@
+using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-
+using Microsoft.Extensions.Configuration;
 
 namespace l2l.Data.Model
 {
@@ -8,11 +10,22 @@ namespace l2l.Data.Model
     {
         public L2lDbContext CreateDbContext(string[] args)
         {
-            var builder = new DbContextOptionsBuilder<L2lDbContext>();
+            var oBuilder = new DbContextOptionsBuilder<L2lDbContext>();
 
-            builder.UseSqlite("Data Source=l2l.db;");
+            var basePath = Directory.GetCurrentDirectory();
 
-            return new L2lDbContext(builder.Options);
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var cBuilder = new ConfigurationBuilder()
+                                .SetBasePath(basePath)
+                                .AddJsonFile("appsettings.json")
+                                .AddJsonFile($"appsettings.{environment}.json")
+                                .AddEnvironmentVariables();
+
+
+            oBuilder.UseSqlite("Data Source=l2l.db;");
+
+            return new L2lDbContext(oBuilder.Options);
         }
     }
 }
