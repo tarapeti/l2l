0fdd3455fd7e5a4f57cb5a396f6196f670a54abf
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-04 10:56:06 +0100

    created context factory

---
+++ l2l.Data/Model/L2lDbContextFactory.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+
+namespace l2l.Data.Model
+{
+    public class L2lDbContextFactory : IDesignTimeDbContextFactory<L2lDbContext>
+    {
+        public L2lDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<L2lDbContext>();
+
+            //builder.UseSqlite("");
+
+            return new L2lDbContext(builder.Options);
+        }
+    }
+}
