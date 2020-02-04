1b97ca0c334d701662af480f401deb20d08510b0
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-04 12:06:02 +0100

    created basic test for add & findbyid

--- l2l.Data.Tests/CourseRepositoryTests.cs
+++ l2l.Data.Tests/CourseRepositoryTests.cs
@@ -1,16 +1,26 @@
 using System;
+using l2l.Data.Model;
 using Xunit;
+using l2l.Data.Repository;
+
 
 namespace l2l.Data.Tests
 {
     /// <summary>
     /// CRUD and list tests
     /// </summary>
-    public class CoureseRepositoryTests
+    public class CourseRepositoryTests
     {
         [Fact]
-        public void CoureseRepositoryTests_CreateCourseShouldBeInRepository()
+        public void CourseRepositoryTests_CreateCourseShouldBeInRepository()
         {
+            var sut = new CourseRepository();
+            var course = new Course{Id = 1, Name="Test course"};
+
+            sut.Add(course);
+            var result = sut.GetById(course.Id);
+
+            Assert.NotNull(result);
 
         }
     }
