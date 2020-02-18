6312e326109cdb55bd21a6bc98fda74a66284835
Taraszovics Péter <gtarapeti@gmail.com>
2020-02-18 10:24:26 +0100

    added global string for datasource

--- l2l.Data/GlobalStrings.cs
+++ l2l.Data/GlobalStrings.cs
@@ -4,6 +4,9 @@ namespace l2l.Data
     {
         public static string CONNECTION_NAME {get;} = "DefaultConnection";
         public static string ASPNETCORE_ENVIRONMENT {get;} = "ASPNETCORE_ENVIRONMENT";
+        public static string DATA_SOURCE {get;} = "Data Source";
+
+
 
     }
 }
